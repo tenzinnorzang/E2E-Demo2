13976ec998ba352d6a730555dd908525f04142e5
Tenzin Norzang <tenzin27norzang@gmail.com>
2023-07-27 10:13:42 +0600

    Just check2

--- cypress/misc/page-objects/DesktopPage.js
+++ cypress/misc/page-objects/DesktopPage.js
@@ -30,7 +30,6 @@ export default class DesktopPage{
             cy.get("#topcartlink").should("contain.text", `Shopping cart (${quantity})`)
         })
     }
-    dsfafds
 
     static sortArray(attr, typeOfSort){
         cy.get(`.product-${attr} > a`)
